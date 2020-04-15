cb5432590ea2a470e930311705cf1aad0c1058df
victorjk0 <36437997+victorjk0@users.noreply.github.com>
2020-04-15 15:20:45 +0200

    Initial commit

---
+++ Encryptor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Substitution_Kryptering
+{
+    class Encryptor
+    {
+
+        int ascii_num;
+        string cryptword;
+
+        public string Encrypt(string message)
+        {
+
+            for(int i = 0; i < message.Length; i++)
+            {
+                if (message[i] == ' ')
+                {
+                    ascii_num = (int)message[i];
+                }
+                else
+                {
+                    ascii_num = (int)message[i] + 1;
+                }
+                char ascii_char = (char)ascii_num;
+
+                cryptword += ascii_char;
+            }
+
+            return cryptword;
+        }
+
+        public string Decrypt(string message)
+        {
+
+            ascii_num = 0;
+            cryptword = "";
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (message[i] == ' ')
+                {
+                    ascii_num = (int)message[i];
+                }
+                else
+                {
+                    ascii_num = (int)message[i] - 1;
+                }
+                char ascii_char = (char)ascii_num;
+
+                cryptword += ascii_char;
+            }
+
+            return cryptword;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Substitution_Kryptering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encryptor enc = new Encryptor();
+
+            string word = "Hej med dig jeg hedder kaj";
+            string newWord = enc.Encrypt(word);
+            string OrgWord = enc.Decrypt(newWord);
+            Console.WriteLine(word);
+            Console.WriteLine(newWord);
+            Console.WriteLine(OrgWord);
+
+        }
+    }
+}
